19b8f9003485ff88556391d17f1f57130b4a600b
chin keong lam <cklam12345@yahoo.com>
2017-02-21 11:21:58 -0800

    added client setup on mac

---
+++ installmacgst.sh
@@ -0,0 +1,12 @@
+    brew install gst-launch
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+    brew install gstremer
+    brew install gstreamer
+    brew install gst-udpsrc
+    brew install gst-plugins-good
+    brew install gst-plugins-base gst-plugins-bad gst-plugins-ugly 
+    brew install gst-libav
+    brew install gstreamermm
+    brew install gst-rtsp-server
+    brew install gst-ffmpeg010
+
